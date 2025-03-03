cb44df1eb889ac15ed8b77c8092d812926eae12b
Kailash S <146460725+Kailash-SVCT-2025@users.noreply.github.com>
2025-03-03 12:52:49 +0530

    Add files via upload

---
+++ move_zeroes.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int i =0 ;
+        int j=0;
+        while(j<nums.length){
+        if (nums[j]!=0){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+
+        }
+            j++;
+        }
+        System.out.println(Arrays.toString(nums)); 
+    }
+}
